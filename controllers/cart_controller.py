ffe78d0c32e448a90433636fcb9ae95b5217bf0e
fishball0741 <fishball0741@gmail.com>
2022-11-08 20:29:59 +1000

    cart done

--- controllers/cart_controller.py
+++ controllers/cart_controller.py
@@ -1,7 +1,7 @@
 from flask import Blueprint, request
 from init import db
 from datetime import date
-from models.product import Product, ProductSchema
+from models.product import ProductSchema
 from models.user import User, UserSchema1
 from models.cart import Cart, CartSchema
 from controllers.user_controller import authorize1, authorize
@@ -9,12 +9,6 @@ from flask_jwt_extended import jwt_required, get_jwt_identity
 
 
 cart_bp = Blueprint('cart', __name__, url_prefix='/cart')
-
-# def authorize2():
-#     user_email = get_jwt_identity()
-#     stmt = db.select(User).filter_by(email=user_email)
-#     user = db.session.scalar(stmt)
-#     return user.email
 
 @cart_bp.route('/')
 @jwt_required()
@@ -39,47 +33,46 @@ def one_user_carts(email):
         return {'error': f"Cart not found with {email}."}, 404
 
 
-#post for adding stuff to the cart
-@cart_bp.route('/<int:id>/', methods=['PUT', 'PATCH'])
+#post for adding product to cart
+@cart_bp.route('/<int:id>/', methods=['POST'])
 @jwt_required()
 def create_cart(id):
-    if not authorize1() == id:
-        return {'error': "You do not have authorization."}, 401
+    data = ProductSchema().load(request.json)
 
-    stmt = db.select(Product).filter_by(id=id)  #specify id = id
-    cart = db.session.scalar(stmt)
-
-    cart = Product(
-        categories = request.json['categories'],
-        name = request.json['name'],
-        description = request.json.get('description'),
-        status = request.json.get('status'),
-        price = request.json['price'],
+    cart = Cart(
+        product_id = data['id'],
+        cart_created_date = date.today(),
         user_id = get_jwt_identity()
-        # categories = request.json.get('categories') or product.categories,
-        # name = request.json.get('name') or product.name,
-        # description = request.json.get('description') or product.description,
-        # status = request.json.get('status') or product.status,
-        # price = request.json.get('price') or product.price,
-        # user_id = get_jwt_identity()
     )
-        # **********error ****
-        # cart = Cart(
-        #     product = request.json.get('product'),
-        #     cart_created_date = date.today(),
-        #     user_id = get_jwt_identity()
-        # )
-        
-        
-    #  Add and commit cart to db
     db.session.add(cart)
     db.session.commit()
-    return CartSchema().dump(cart), 201   #dump = out
+    return CartSchema().dump(cart), 201
 
 
-@cart_bp.route('/<int:id>/', methods=['PUT', 'PATCH'])
+# check each cart by user's email and cart id.
+@cart_bp.route('/<string:email>/<int:id>/')
 @jwt_required()
-def update_cart(id):
-    if not authorize1() == id:
-        return {'error': "You do not have authorization."}, 401
+def one_cart(email, id):
+    stmt = db.select(User).filter_by(email=email)
+    stmt = db.select(Cart).filter_by(id=id)
+    cart = db.session.scalar(stmt)
+    if cart:
+        return CartSchema().dump(cart)
+    else:
+        return {'error': f"Cart not found with id {id}."}, 404
+
+#Delete a cart by user's email and cart id
+@cart_bp.route('/<string:email>/<int:id>/', methods=['DELETE'])
+@jwt_required()
+def delete_cart(email, id):
+    stmt = db.select(User).filter_by(email=email)
+    stmt = db.select(Cart).filter_by(id=id)
+    cart = db.session.scalar(stmt)
+
+    if cart:
+        db.session.delete(cart)
+        db.session.commit()
+        return {'message': "Cart deleted successfully."}, 200
+    else:
+        return {'error': f"Cart not found with id {id}."}, 404
     
